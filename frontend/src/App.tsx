0fd1b5ab24c8c06c469b69d3b1fa1eaf4f94336a
Alberto Perez <331258@NTTDATA.COM>
2024-12-24 15:54:53 -0600

    tittle added: V3.1.2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -39,8 +39,10 @@ function App() {
 
 
   return (
-    <div className=' mt-20 flex justify-center align-middle w-screen h-screen'>
-
+    <div className=' mt-20 flex flex-col justify-start text-center items-center w-screen h-screen'>
+      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-slate-500 from-slate-700">Bcycle madisson stations</span>
+      </h1>
       <div className=" relative flex flex-col w-4/6 h-3/4 overflow-hidden text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
         <div className="ml-3 mt-3">
           <div className="w-full max-w-sm min-w-[200px] relative">
